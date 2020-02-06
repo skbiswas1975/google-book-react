f5c772a21d417a617563ebf6251fe92b6caa5ef2
S Biswas <sbiswas1975@gmail.com>
2020-02-06 22:49:29 +0530

    initial design completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class BookSearch extends Component {
+  render() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">Search Books</a></li>
+        <li><a href="#services">Saved Books</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron text-left">
+  <h2>Google Book Search using React</h2> 
+   
+  <form>
+    <div class="form-group">
+      <input type="text" class="form-control" size="50" placeholder="Search for books" autoComplete="off" />
+      </div>
+        <button type="submit" class="btn btn-danger">Submit</button>
+  </form>
+</div>
+
+
+
+
+
+
+
+  
+  
+
+
+
+<footer class="container-fluid text-center">
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <p>Copyright</p>
+</footer>
+
+
+
     </div>
   );
 }
+}
 
-export default App;
+export default BookSearch;
