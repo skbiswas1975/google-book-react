39cd1d52880feeeb208f6b0a79bfc1a38bc02121
skbiswas1975 <you@example.comsbiswas1975@gmail.com>
2020-02-10 01:09:14 +0530

    no saved books message updated

--- client/src/components/SavedResult/index.js
+++ client/src/components/SavedResult/index.js
@@ -5,13 +5,13 @@ import {Row, Col} from "../Grid"
 const SavedResult = props => {
     return (props.savedBooks.length === 0) ? (
         <div className="card">
-        {/*
+        
             <div className="card-body player">
                 <div className="article">
-                    <h3>Books that You Saved</h3>
+                    <h3>No Saved Books</h3>
                 </div>
             </div>
-        */}
+        
         </div>
     ):(
         <div className="card">
